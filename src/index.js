53a33a0d08680d02778a1ba4fe0b5fe25faaa40a
#云淡然 <cloudcome@163.com>
2016-05-19 11:44:35 +0800

    blear.core.animation

--- src/index.js
+++ src/index.js
@@ -5,139 +5,129 @@
  */
 
 
-    /**
-     * @module core/animation
-     * @requires utils/access
-     * @requires utils/easing
-     * @requires utils/typeis
-     * @requires utils/object
-     * @requires utils/time
-     * @requires utils/date
-     * @requires utils/function
-     * @requires core/attribute
-     */
 
-    'use strict';
 
-    var access = require('../utils/access.js');
-    var easing = require('../utils/easing.js');
-    var typeis = require('../utils/typeis.js');
-    var object = require('../utils/object.js');
-    var time = require('../utils/time.js');
-    var date = require('../utils/date.js');
-    var fun = require('../utils/function.js');
-    var attribute = require('./attribute.js');
+'use strict';
 
-    var defaults = exports.defaults = {
-        easing: 'linear',
-        duration: 345
+var access =     require('blear.utils.access');
+var easing =     require('blear.utils.easing');
+var typeis =     require('blear.utils.typeis');
+var object =     require('blear.utils.object');
+var time =       require('blear.utils.time');
+var date =       require('blear.utils.date');
+var fun =        require('blear.utils.function');
+var attribute =  require('blear.core.attribute');
+
+var defaults = exports.defaults = {
+    easing: 'linear',
+    duration: 345
+};
+
+/**
+ * 获取样式
+ * @param el
+ * @param cssKey
+ * @returns {Number}
+ */
+var getStyle = function (el, cssKey) {
+    return parseFloat(attribute.style(el, cssKey));
+};
+
+
+/**
+ * 设置样式
+ * @param el
+ * @param styles
+ */
+var setStyles = function (el, styles) {
+    attribute.style(el, styles);
+};
+
+
+var animation = function (el, options, onTransition, onTransitionEnd) {
+    options = object.assign({}, defaults, options);
+
+    var optEasing = options.easing;
+    var ease = easing[optEasing] || easing[defaults.easing];
+    var startTime = date.now();
+    var duration = options.duration;
+
+    var flash = function () {
+        time.nextFrame(function () {
+            var past = date.now() - startTime;
+
+            if (past >= duration) {
+                onTransitionEnd();
+                return;
+            }
+
+            var timeRatio = past / duration;
+
+            onTransition(ease(timeRatio));
+
+            flash();
+        });
     };
 
-    /**
-     * 获取样式
-     * @param el
-     * @param cssKey
-     * @returns {Number}
-     */
-    var getStyle = function (el, cssKey) {
-        return parseFloat(attribute.style(el, cssKey));
-    };
+    flash();
+};
 
 
-    /**
-     * 设置样式
-     * @param el
-     * @param styles
-     */
-    var setStyles = function (el, styles) {
-        attribute.style(el, styles);
-    };
+/**
+ * 动画
+ * @param el {HTMLElement} 元素
+ * @param to {Object} 终点
+ * @param [options] {Object} 配置
+ * @param [options.duration] {Number} 动画时间
+ * @param [options.easing] {Number} 动画缓冲
+ * @param [callback] {Function} 回调
+ */
+exports.animate = function (el, to, options, callback) {
+    var args = access.args(arguments);
+    var argLen = args.length;
+    // 最后一个参数是否为 Function
+    var hasCallback = typeis.Function(args[argLen - 1]);
 
+    switch (argLen) {
+        case 3:
+            // .animate(el, to, callback);
+            if (hasCallback) {
+                options = args[3];
+                callback = args[2];
+            }
+            //  .animate(el, to, options);
 
-    var animation = function (el, options, onTransition, onTransitionEnd) {
-        options = object.assign({}, defaults, options);
+            break;
 
-        var optEasing = options.easing;
-        var ease = easing[optEasing] || easing[defaults.easing];
-        var startTime = date.now();
-        var duration = options.duration;
+        case 2:
+            // .animate(el, to);
+            options = args[2];
+            break;
+    }
 
-        var flash = function () {
-            time.nextFrame(function () {
-                var past = date.now() - startTime;
+    callback = fun.noop(callback);
+    var animateMap = {};
 
-                if (past >= duration) {
-                    onTransitionEnd();
-                    return;
-                }
+    object.each(to, function (cssKey, cssVal) {
+        var start = getStyle(el, cssKey);
+        var end = parseFloat(cssVal);
 
-                var timeRatio = past / duration;
-
-                onTransition(ease(timeRatio));
-
-                flash();
-            });
+        animateMap[cssKey] = {
+            s: start,
+            l: end - start
         };
+    });
 
-        flash();
-    };
+    animation(el, options, function (ratio) {
+        var styles = {};
 
-
-    /**
-     * 动画
-     * @param el {HTMLElement} 元素
-     * @param to {Object} 终点
-     * @param [options] {Object} 配置
-     * @param [options.duration] {Number} 动画时间
-     * @param [options.easing] {Number} 动画缓冲
-     * @param [callback] {Function} 回调
-     */
-    exports.animate = function (el, to, options, callback) {
-        var args = access.args(arguments);
-        var argLen = args.length;
-        // 最后一个参数是否为 Function
-        var hasCallback = typeis.Function(args[argLen - 1]);
-
-        switch (argLen) {
-            case 3:
-                // .animate(el, to, callback);
-                if (hasCallback) {
-                    options = args[3];
-                    callback = args[2];
-                }
-                //  .animate(el, to, options);
-
-                break;
-
-            case 2:
-                // .animate(el, to);
-                options = args[2];
-                break;
-        }
-
-        callback = fun.noop(callback);
-        var animateMap = {};
-
-        object.each(to, function (cssKey, cssVal) {
-            var start = getStyle(el, cssKey);
-            var end = parseFloat(cssVal);
-
-            animateMap[cssKey] = {
-                s: start,
-                l: end - start
-            };
+        object.each(animateMap, function (cssKey, meta) {
+            styles[cssKey] = meta.l * ratio + meta.s;
         });
 
-        animation(el, options, function (ratio) {
-            var styles = {};
-
-            object.each(animateMap, function (cssKey, meta) {
-                styles[cssKey] = meta.l * ratio + meta.s;
-            });
-
-            setStyles(el, styles);
-        }, function () {
-            setStyles(el, to);
-            callback();
-        });
-    };
+        setStyles(el, styles);
+    }, function () {
+        setStyles(el, to);
+        callback();
+    });
+};
